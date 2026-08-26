60c3712275426536edd0d00ed83224325f5ae4b6
Khushi Sharma <khushiiee.26@gmail.com>
2026-08-27 00:12:19 +0530

    Time: 2 ms (77.23%), Space: 7.8 MB (65.67%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int fib(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int prev = 0;
+        int next = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int result = prev + next;
+
+            prev = next;
+            next = result;
+        }
+
+        return next;
+    }
+};
